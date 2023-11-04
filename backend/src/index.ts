13c55069fb642f40fed19c1a8edc649e4c4b2cca
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-05 03:47:24 +0600

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,18 @@
 
+import app from './app';
+import { connectToDatabase } from './db/conection';
+import { config } from 'dotenv'
+config();
+
+const PORT = process.env.PORT || 5000;
+
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT} and connected to MONGODB`);
+    });
+  })
+  .catch((error) => {
+    console.error(`Error connecting to the database: ${error.message}`);
+    process.exit(1);
+  });
