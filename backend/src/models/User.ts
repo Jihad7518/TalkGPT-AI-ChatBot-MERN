a6c5285b525ef1a181e9b6387edfdb5093875a64
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-06 00:33:33 +0600

    Update User.ts

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -1,1 +1,37 @@
 
+import mongoose from "mongoose";
+import { randomUUID } from "crypto";
+
+const chatSchema = new mongoose.Schema({
+    id: {
+        type: "string",
+        default: randomUUID(),
+    },
+    role: {
+        type: "string",
+        required: true,
+    },
+    content: {
+        type: "string",
+        required: true,
+    },
+})
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: "string",
+        required: true
+    },
+    email: {
+        type: "string",
+        required: true,
+        unique: true
+    },
+    password: {
+        type: "string",
+        required: true,
+    },
+    chats: [chatSchema],
+});
+
+export default mongoose.model("User", userSchema);
