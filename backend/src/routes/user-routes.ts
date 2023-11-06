2bfee90c08adf7b89461ca9d679fd1d99753f422
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-07 01:08:19 +0600

    Update user-routes.ts

--- backend/src/routes/user-routes.ts
+++ backend/src/routes/user-routes.ts
@@ -1,1 +1,17 @@
 
+import { Router } from "express";
+import {
+  getAllUsers,
+  userLogin,
+  userLogout,
+  userSignup,
+  verifyUser,
+} from "../controllers/user-controllers.js";
+import {
+  loginValidator,
+  signupValidator,
+  validate,
+} from "../utils/validators.js";
+import { verifyToken } from "../utils/token-manager.js";
+
+const userRoutes = Router();
