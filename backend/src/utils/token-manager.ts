9eb6c913ab70f428cfa4ed5ed29b2901371849fa
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-07 01:12:56 +0600

    Update token-manager.ts

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -1,4 +1,3 @@
-
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { COOKIE_NAME } from "./constants.js";
@@ -16,3 +15,20 @@ export const verifyToken = async (
   res: Response,
   next: NextFunction
 ) => {
+  const token = req.signedCookies[`${COOKIE_NAME}`];
+  if (!token || token.trim() === "") {
+    return res.status(401).json({ message: "Token Not Received" });
+  }
+  return new Promise<void>((resolve, reject) => {
+    return jwt.verify(token, process.env.JWT_SECRET, (err, success) => {
+      if (err) {
+        reject(err.message);
+        return res.status(401).json({ message: "Token Expired" });
+      } else {
+        resolve();
+        res.locals.jwtData = success;
+        return next();
+      }
+    });
+  });
+};
