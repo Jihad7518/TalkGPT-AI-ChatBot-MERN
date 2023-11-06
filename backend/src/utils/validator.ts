2f675c4a9450f847490c8920a46b8c538c156a03
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-07 01:13:19 +0600

    Update validator.ts

--- backend/src/utils/validator.ts
+++ backend/src/utils/validator.ts
@@ -1,4 +1,3 @@
-
 import { NextFunction, Request, Response } from "express";
 import { body, ValidationChain, validationResult } from "express-validator";
 
@@ -17,3 +16,20 @@ export const validate = (validations: ValidationChain[]) => {
     return res.status(422).json({ errors: errors.array() });
   };
 };
+
+export const loginValidator = [
+  body("email").trim().isEmail().withMessage("Email is required"),
+  body("password")
+    .trim()
+    .isLength({ min: 6 })
+    .withMessage("Password should contain atleast 6 characters"),
+];
+
+export const signupValidator = [
+  body("name").notEmpty().withMessage("Name is required"),
+  ...loginValidator,
+];
+
+export const chatCompletionValidator = [
+  body("message").notEmpty().withMessage("Message  is required"),
+];
