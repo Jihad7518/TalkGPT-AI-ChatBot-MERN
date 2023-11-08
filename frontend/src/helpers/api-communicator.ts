1b436a85bea13e89d1a80983183591ddd44e0f2e
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-09 01:36:46 +0600

    Update api-communicator.ts

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -1,1 +1,23 @@
 
+import axios from "axios";
+export const loginUser = async (email: string, password: string) => {
+  const res = await axios.post("/user/login", { email, password });
+  if (res.status !== 200) {
+    throw new Error("Unable to login");
+  }
+  const data = await res.data;
+  return data;
+};
+
+export const signupUser = async (
+  name: string,
+  email: string,
+  password: string
+) => {
+  const res = await axios.post("/user/signup", { name, email, password });
+  if (res.status !== 201) {
+    throw new Error("Unable to Signup");
+  }
+  const data = await res.data;
+  return data;
+};
