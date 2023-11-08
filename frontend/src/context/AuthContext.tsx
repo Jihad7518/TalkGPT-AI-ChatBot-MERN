fca762ee98ed24f2bb865c320210e8115dcae07e
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-09 01:35:36 +0600

    Update AuthContext.tsx

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,1 +1,26 @@
 
+import {
+    ReactNode,
+    createContext,
+    useContext,
+    useEffect,
+    useState,
+  } from "react";
+  import {
+    checkAuthStatus,
+    loginUser,
+    logoutUser,
+    signupUser,
+  } from "../helpers/api-communicator";
+  
+  type User = {
+    name: string;
+    email: string;
+  };
+  type UserAuth = {
+    isLoggedIn: boolean;
+    user: User | null;
+    login: (email: string, password: string) => Promise<void>;
+    signup: (name: string, email: string, password: string) => Promise<void>;
+    logout: () => Promise<void>;
+  };
