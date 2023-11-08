3fef7cb6dfd66dc07f1f8e448368c8da2ce15618
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-09 01:33:49 +0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,12 @@
 
+import Header from "./components/Header";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Chat from "./pages/Chat";
+import NotFound from "./pages/NotFound";
+import { useAuth } from "./context/AuthContext";
+
+function App() {
+  const auth = useAuth();
