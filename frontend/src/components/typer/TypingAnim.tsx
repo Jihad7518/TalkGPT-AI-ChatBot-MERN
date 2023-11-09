4ce01d34898f7b99ec4762b2ceda6d02d0b4e983
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-10 00:37:22 +0600

    Update TypingAnim.tsx

--- frontend/src/components/typer/TypingAnim.tsx
+++ frontend/src/components/typer/TypingAnim.tsx
@@ -1,1 +1,30 @@
 
+import { TypeAnimation } from "react-type-animation";
+
+const TypingAnim = () => {
+  return (
+    <TypeAnimation
+      sequence={[
+        // Same substring at the start will only be typed once, initially
+        "Chat With Your OWN AI",
+        1000,
+        "Built With OpenAI 🤖",
+        2000,
+        "Your Own Customized ChatGPT 💻",
+        1500,
+        "Take Love From Mr. Jihad ❤",
+        2500,
+      ]}
+      speed={50}
+      style={{
+        fontSize: "60px",
+        color: "white",
+        display: "inline-block",
+        textShadow: "1px 1px 20px #000",
+      }}
+      repeat={Infinity}
+    />
+  );
+};
+
+export default TypingAnim;
