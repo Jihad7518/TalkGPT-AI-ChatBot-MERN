4639dae62f9ed5a1d5eeb657fcad21a18bebdc55
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-10 00:38:56 +0600

    Update Logo.tsx

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,1 +1,24 @@
 
+import React from 'react';
+import { Link } from "react-router-dom";
+import Typography from "@mui/material/Typography";
+
+const Logo = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        marginRight: "auto",
+        alignItems: "center",
+        gap: "15px",
+      }}
+    >
+      <Link to={"/"}>
+        <img
+          src="openai.png"
+          alt="openai"
+          width={"30px"}
+          height={"30px"}
+          className="image-inverted"
+        />
+      </Link>{" "}
