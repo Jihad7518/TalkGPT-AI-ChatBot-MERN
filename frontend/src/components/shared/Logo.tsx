d0b91401f49b828f6da1818613b549cd6279f361
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-10 00:39:06 +0600

    Update Logo.tsx

--- frontend/src/components/shared/Logo.tsx
+++ frontend/src/components/shared/Logo.tsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { Link } from "react-router-dom";
 import Typography from "@mui/material/Typography";
@@ -22,3 +21,18 @@ const Logo = () => {
           className="image-inverted"
         />
       </Link>{" "}
+      <Typography
+        sx={{
+          display: { md: "block", sm: "none", xs: "none" },
+          mr: "auto",
+          fontWeight: "800",
+          textShadow: "2px 2px 20px #000",
+        }}
+      >
+        <span style={{ fontSize: "20px" }}>TALK</span>-GPT
+      </Typography>
+    </div>
+  )
+}
+
+export default Logo;
