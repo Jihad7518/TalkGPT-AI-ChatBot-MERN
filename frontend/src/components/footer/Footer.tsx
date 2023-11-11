1d5790531e6c37b92720701ad0b04db253982989
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-11 23:24:15 +0600

    Update Footer.tsx

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -1,1 +1,33 @@
 
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+        © Built by
+          <span>
+            <Link
+              style={{ color: "white" }}
+              className="nav-link"
+              to={"https://www.linkedin.com/in/md-jihad-8685a424a/"}
+            >
+              Md. Jihad
+            </Link>
+          </span>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
