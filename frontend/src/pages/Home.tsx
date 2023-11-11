bd0bf67ccd9139dbb611a4c16c5b8b5b60f47b89
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-12 01:07:26 +0600

    Update Home.tsx

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,1 +1,24 @@
 
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import React from "react";
+import TypingAnim from "../components/typer/TypingAnim";
+import Footer from "../components/footer/Footer";
+
+const Home = () => {
+  const theme = useTheme();
+  const isBelowMd = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <Box width={"100%"} height={"100%"}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          mx: "auto",
+          mt: 3,
+        }}
+      >
+        <Box>
+          <TypingAnim />
+        </Box>
