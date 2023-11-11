52df05f7200bca9953a368e81f5ff29f3df31014
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-12 01:08:07 +0600

    Update Chat.tsx

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,1 +1,18 @@
 
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Box, Avatar, Typography, Button, IconButton } from "@mui/material";
+import red from "@mui/material/colors/red";
+import { useAuth } from "../context/AuthContext";
+import ChatItem from "../components/chat/ChatItem";
+import { IoMdSend } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+import {
+  deleteUserChats,
+  getUserChats,
+  sendChatRequest,
+} from "../helpers/api-communicator";
+import toast from "react-hot-toast";
+type Message = {
+  role: "user" | "assistant";
+  content: string;
+};
