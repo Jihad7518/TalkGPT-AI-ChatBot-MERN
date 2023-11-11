fb9d26e8ab8ad7b6aae59b0b6bc5a2d0b8f18eb6
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-11-11 23:25:56 +0600

    Update ChatItem.tsx

--- frontend/src/components/chat/ChatItem.tsx
+++ frontend/src/components/chat/ChatItem.tsx
@@ -53,3 +53,46 @@ const ChatItem = ({
         {!messageBlocks && (
           <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
         )}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={coldarkDark} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography sx={{ fontSize: "20px" }}>{block}</Typography>
+            )
+          )}
+      </Box>
+    </Box>
+  ) : (
+    <Box
+      sx={{
+        display: "flex",
+        p: 2,
+        bgcolor: "#004d56",
+        gap: 2,
+        borderRadius: 2,
+      }}
+    >
+      <Avatar sx={{ ml: "0", bgcolor: "black", color: "white" }}>
+        {auth?.user?.name[0]}
+        {auth?.user?.name.split(" ")[1][0]}
+      </Avatar>
+      <Box>
+        {!messageBlocks && (
+          <Typography sx={{ fontSize: "20px" }}>{content}</Typography>
+        )}
+        {messageBlocks &&
+          messageBlocks.length &&
+          messageBlocks.map((block) =>
+            isCodeBlock(block) ? (
+              <SyntaxHighlighter style={coldarkDark} language="javascript">
+                {block}
+              </SyntaxHighlighter>
+            ) : (
+              <Typography sx={{ fontSize: "20px" }}>{block}</Typography>
+            )
+          )}
+      </Box>
